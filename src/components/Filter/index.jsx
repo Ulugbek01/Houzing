1e049fd0f82161711fb63607d63e74fc97e57b92
Ulugbek01 <xyz207820@gmail.com>
2022-05-24 15:28:55 +0500

    Filter section add

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,13 +1,45 @@
+import { Popover } from 'antd'
 import React from 'react'
 import { Button } from '../Generic/Button'
 import { Input } from '../Generic/Input'
-import { Container, FilterIcon, SearchIcon } from './style'
+import { Advanced, Container, FilterIcon, SearchIcon, Section } from './style'
 
 export const Filter = () => {
+  const advancedSearch = (
+    <Advanced>
+      <Advanced.Body>
+      <Advanced.Title>Address</Advanced.Title>
+      <Section>
+        <Input placeholder="Country"/>
+        <Input placeholder="Region"/>
+        <Input placeholder="City"/>
+        <Input placeholder="Zip code"/>
+      </Section>
+      <Advanced.Title>Apartment info</Advanced.Title>
+      <Section>
+        <Input placeholder="Rooms"/>
+        <Input placeholder="Size"/>
+        <Input placeholder="Sort"/>
+      </Section>
+      <Advanced.Title>Price</Advanced.Title>
+      <Section>
+        <Input placeholder="Min price"/>
+        <Input placeholder="Max price"/>
+      </Section>
+      </Advanced.Body>
+    
+      <Advanced.Footer>
+        <Button type="secondary" width={128} mr={20}>Cancel</Button>
+        <Button type="primary" width={128}>Submit</Button>
+      </Advanced.Footer>
+    </Advanced>
+  )
   return (
     <Container>
         <Input placeholder="Enter an address, neighborhood, city, or ZIP code"/>
-        <Button type='secondary' width='131' mr={20} ml={20}><FilterIcon/> Advanced</Button>
+        <Popover placement='bottomRight' content={advancedSearch} trigger="click">
+          <Button type='secondary' width='131' mr={20} ml={20}><FilterIcon/> Advanced</Button>
+        </Popover>
         <Button type='primary' width='180'><SearchIcon/> Search</Button>
     </Container>
   )
--- src/components/Filter/style.js
+++ src/components/Filter/style.js
@@ -17,3 +17,31 @@ export const SearchIcon = styled(searchIcon)`
 export const FilterIcon = styled(filterIcon)`
   margin-right: 8px;
 `;
+
+export const Advanced = styled.div``;
+
+Advanced.Body = styled.div`
+  padding: 10px;
+`;
+
+Advanced.Title = styled.h4`
+  font-family: "Montserrat", sans-serif;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 24px;
+  color: var(--secondary-text-color);
+`;
+
+export const Section = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px 20px;
+  margin-bottom: 20px;
+`;
+
+Advanced.Footer = styled.div`
+  display: flex;
+  align-items: center;
+  background: #f6f8f9;
+  padding: 20px;
+`;
