5b56743d21f0afa36ed8ed4aa2e6ba6c45b1c230
Ulugbek01 <xyz207820@gmail.com>
2022-05-25 08:07:24 +0500

    Main section corusel add

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,14 +1,49 @@
 import React from 'react'
-import { Container, MainSection } from './style';
+import { Container} from './style';
 import { Filter } from '../Filter';
+// import { Button } from '../Generic/Button';
+import { Carousel } from 'antd';
+import mainImage from "../../assets/images/home-img.jpg";
 
+const contentStyle = {
+  minHeight: '570px',
+  color: '#fff',
+  textAlign: 'center',
+};
 
 export const Home = () => {
   return (
     <Container>
       <Filter/>
-      <MainSection>
-      </MainSection>
+        <Carousel autoplay>
+          {/* <MainSection>
+            <MainSection.Overflow>
+              <MainSection.Title>Skyper Pool Partment</MainSection.Title>
+              <MainSection.Discription className="section-discription">112 Glenwood Ave Hyde Park, Boston, MA</MainSection.Discription>
+              <Button width={180}>Read more</Button>
+            </MainSection.Overflow>
+          </MainSection> */}
+        <div>
+          <h3 style={contentStyle}>
+            <img src={mainImage} alt="main-image" />
+          </h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>
+            <img src={mainImage} alt="main-image" />
+          </h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>
+            <img src={mainImage} alt="main-image" />
+          </h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>
+            <img src={mainImage} alt="main-image" />
+          </h3>
+        </div>
+        </Carousel>
     </Container>
   )
 }
--- src/components/Home/style.js
+++ src/components/Home/style.js
@@ -4,18 +4,33 @@ import mainImage from "../../assets/images/home-img.jpg";
 export const Container = styled.div``;
 
 export const MainSection = styled.div`
-  background: url(${mainImage}) no-repeat center bottom;
-  background-size: cover;
+  /* background: url(${mainImage}) no-repeat center bottom; */
+  /* background-size: cover; */
   min-height: 570px;
   position: relative;
+  text-align: center;
+`;
 
-  &::after {
-    content: "";
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.65);
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
+MainSection.Overflow = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.65);
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 111;
+`;
+
+MainSection.Title = styled.h2`
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 44px;
+  line-height: 48px;
+  letter-spacing: -0.02em;
+  color: var(--primary-text-color);
+`;
+
+MainSection.Discription = styled.p`
+  color: #fff;
 `;
