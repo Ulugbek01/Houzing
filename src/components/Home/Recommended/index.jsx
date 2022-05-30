0db84c676d79aacd1d96584112238e31918acca0
Ulugbek01 <xyz207820@gmail.com>
2022-05-30 19:43:19 +0500

    Recommended section add

---
+++ src/components/Home/Recommended/index.jsx
@@ -0,0 +1,47 @@
+import React,{useRef} from 'react'
+import AliceCarousel from 'react-alice-carousel';
+import { Card } from '../../Generic/Card';
+import {CardsWrapper, NextIconWrapper, RecommendedWrapper } from './style';
+import {ReactComponent as ArrowLeft} from '../../../assets/icons/left-arrow.svg';
+import {ReactComponent as ArrowRight} from '../../../assets/icons/right-arrow.svg';
+
+const responsive = {
+    0: { items: 1 },
+    576: { items: 2 },
+    992: { items: 3 },
+  };
+
+export const Recommended = () => {
+  const slider = useRef();
+  const items = [
+    <Card/>,
+    <Card/>,
+    <Card/>,
+    <Card/>,
+    <Card/>
+  ]
+
+  return (
+        <RecommendedWrapper className='nocopy'>
+            <RecommendedWrapper.Title className="section-title text-center">Recommended</RecommendedWrapper.Title>
+            <RecommendedWrapper.Discription className="text-center">Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.</RecommendedWrapper.Discription>
+            <CardsWrapper>
+                <NextIconWrapper position="left" onClick={()=> slider.current?.next()}>
+                    <ArrowLeft/>
+                </NextIconWrapper>
+                <NextIconWrapper position="right" onClick={()=> slider.current?.prev()}>
+                    <ArrowRight/>
+                </NextIconWrapper>
+                <AliceCarousel
+                    ref={slider}
+                    responsive={responsive}
+                    mouseTracking
+                    items={items}
+                    controlsStrategy="alternate"
+                />
+                </CardsWrapper>
+      </RecommendedWrapper>
+  )
+}
+
+export default Recommended;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,54 +1,15 @@
-import React, { useRef } from 'react'
-import AliceCarousel from 'react-alice-carousel';
-import { Card } from '../Generic/Card';
+import React from 'react'
 import { Filter } from '../Filter';
 import MainCarousel from './Carousel';
-
-import {CardsWrapper, Container, NextIconWrapper, RecommendedWrapper } from './style';
-import {ReactComponent as ArrowLeft} from '../../assets/icons/left-arrow.svg';
-import {ReactComponent as ArrowRight} from '../../assets/icons/right-arrow.svg';
-
-
-const responsive = {
-  0: { items: 1 },
-  576: { items: 2 },
-  992: { items: 3 },
-};
+import Recommended from './Recommended';
+import { Container } from './style';
 
 export const Home = () => {
-  const slider = useRef();
-  const items = [
-    <Card/>,
-    <Card/>,
-    <Card/>,
-    <Card/>,
-    <Card/>
-  ]
   return (
     <Container>
       <Filter/>
-      
       <MainCarousel/>
-
-      <RecommendedWrapper className='nocopy'>
-        <RecommendedWrapper.Title className="section-title text-center">Recommended</RecommendedWrapper.Title>
-        <RecommendedWrapper.Discription className="text-center">Nulla quis curabitur velit volutpat auctor bibendum consectetur sit.</RecommendedWrapper.Discription>
-        <CardsWrapper>
-          <NextIconWrapper position="left" onClick={()=> slider.current?.next()}>
-            <ArrowLeft/>
-          </NextIconWrapper>
-          <NextIconWrapper position="right" onClick={()=> slider.current?.prev()}>
-            <ArrowRight/>
-          </NextIconWrapper>
-          <AliceCarousel
-            ref={slider}
-            responsive={responsive}
-            mouseTracking
-            items={items}
-            controlsStrategy="alternate"
-          />
-        </CardsWrapper>
-      </RecommendedWrapper>
+      <Recommended/>
     </Container>
   )
 }
